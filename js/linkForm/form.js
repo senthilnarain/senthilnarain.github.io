28456be0d3bdf2808af636250a1b56ad61659813
senthilnarain <senthil.narain25@gmail.com>
2018-01-20 19:40:57 +0530

    deleted bharath footwear files from root directory

--- js/linkForm/form.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$("#homeContainer").load("screens/home/homeContent");
-    $("#menContainer").load("screens/men/menContent");
-	$("#womenContainer").load("screens/women/womenContent");
-	$("#kidsContainer").load("screens/kids/kidsContent");
-	$("#contactContainer").load("screens/contact/contactContent");
-});
